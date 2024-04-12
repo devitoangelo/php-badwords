3e2345fb33dc3a9b20c21a6d8b4c3c6e222724d5
devitoangelo <angelodevito4@gmail.com>
2024-04-12 18:26:21 +0200

    1 milestone completed

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php-badwords</title>
 </head>
 
 <body>
- 
+
+    <form action="script.php" method="get">
+        <!-- collego il form al file script.php con metodo get-->
+        <input type="text" placeholder="Scrivi una frase" name="name">
+        <!-- parola da stampare a schermo -->
+        <button type="submit">Play</button>
+    </form>
+
 </body>
 
 </html>
---
+++ script.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <h1>
+        <?php
+
+        echo $name = $_GET["name"];
+
+        echo strlen($name);
+        //  stampiamo a schermo cio che scriviamo nell'input
+        // strlen serve per capire la lunghezza 
+        ?> </h1>
+
+
+
+
+    <?php
+
+
+    ?>
+
+
+
+
+</body>
+
+</html>
